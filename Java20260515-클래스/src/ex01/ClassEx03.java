bf17b9736bd3cc97c4376046c8819df7e46a5e1b
kimdaechul <rlaeocjf@gmail.com>
2026-05-15 16:51:12 +0900

    commit

--- Java20260515-클래스/src/ex01/ClassEx03.java
+++ Java20260515-클래스/src/ex01/ClassEx03.java
@@ -7,8 +7,23 @@ public class ClassEx03 {
 
 	public static void main(String[] args) {
 		
+		int age;
+		age = 20;
+		
+		int age2 = 20;
+		
 		//객체 생성, 인스턴스 생성
-		Member hong = new Member();  		
+		Member hong = new Member(); 
+		hong.name = "홍길동";
+		hong.age = 20;
+		hong.phone = "010-1111-2222";
+		hong.memberInfo();
+		
+		Member lee = new Member();
+		lee.name = "이순신";
+		lee.age = 30;
+		lee.phone = "010-2222-3333";
+		lee.memberInfo();
 	}
 }
 
@@ -22,7 +37,7 @@ class Member{
 	int age;
 	String phone;	
 	
-	void memberInfo(String name, int age, String phone) {
+	void memberInfo() {
 		System.out.println("이름 : " + name);
 		System.out.println("나이 : " + age);
 		System.out.println("전화번호 : " + phone);
---
+++ Java20260515-클래스/src/ex02/ClassEx04.java
@@ -0,0 +1,52 @@
+package ex02;
+
+/*
+ * 학생 정보 관리 프로그램
+ */
+public class ClassEx04 {
+
+	public static void main(String[] args) {
+		
+		int age;
+		age = 20;
+		
+		int age2 = 20;
+		
+		//객체 생성, 인스턴스 생성
+		Member hong = new Member("홍길동", 20, "010-1111-2222"); 
+		
+		hong.memberInfo();
+		
+		Member lee = new Member("이순신", 20, "010-2222-3333");
+		
+		lee.memberInfo();
+	}
+}
+
+/*
+ * 클래스는 
+ *    변수(멤버변수)
+ *    메소드(함수)
+ * 멤버변수 앞에 private붙이면 그 멤버변수는 외부공개안함
+ * 내부 클래스에서만 사용가능
+ * 멤버변수는 외부 공개 안하는 것이 원칙 
+ */
+class Member{
+	private String name;
+	private int age;
+	private String phone;	
+	
+	//생성자 -> 멤버변수 초기화 용도
+	public Member(String n,int a, String p) {
+		name = n;
+		age = a;
+		phone = p;		
+	}	
+	
+	void memberInfo() {
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		System.out.println("전화번호 : " + phone);
+	}
+	
+}
