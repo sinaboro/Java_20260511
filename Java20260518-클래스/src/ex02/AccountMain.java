38cc76146779fa435c6dad534e354d940ef6db57
kimdaechul <rlaeocjf@gmail.com>
2026-05-18 10:03:59 +0900

    commit

---
+++ Java20260518-클래스/src/ex02/AccountMain.java
@@ -0,0 +1,36 @@
+package ex02;
+
+public class AccountMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이순신 거래 내역");
+		Account lee = new Account();
+		
+		lee.deposit(15000);
+		lee.withdraw(8000);
+		System.out.println("이순신 현재 잔고: " + lee.balance);
+		
+		System.out.println("홍길동 잔고 통장 입출금 내역");
+		Account hong  = new Account();
+		hong.deposit(10000);
+		hong.withdraw(7000);
+		System.out.println("홍길동 현재 잔고: " + hong.balance);
+		
+
+	}
+
+}
+
+class Account{
+	int balance = 0;
+	
+	void deposit(int amount) { //입금
+		balance += amount;
+	}
+	
+	void withdraw(int amount) { //출금
+		balance -=amount; 
+	}	
+	
+}
