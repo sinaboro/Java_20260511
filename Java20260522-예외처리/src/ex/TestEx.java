2b07cfda6b6c2b3569bfa0f3f28914eefe60fb59
kimdaechul <rlaeocjf@gmail.com>
2026-05-22 11:38:42 +0900

    commit

--- Java20260522-예외처리/src/ex/TestEx.java
+++ Java20260522-예외처리/src/ex/TestEx.java
@@ -8,28 +8,23 @@ public class TestEx {
 
 	public static void main(String[] args) {
 		
-		  try {
+	  try {
+            // 파일 열기
+            FileReader fr = new FileReader("src/ex/test.txt");
+            // 한 글자씩 읽기
+            int data;
+            while ((data = fr.read()) != -1) {
 
-	            // 파일 열기
-	            FileReader fr = new FileReader("src/ex/test.txt");
-
-	            // 한 글자씩 읽기
-	            int data;
-
-	            while ((data = fr.read()) != -1) {
-
-	                System.out.print((char)data);
-	            }
-
-	            // 파일 닫기
-	            fr.close();
-
-	            System.out.println("\n파일 읽기 완료");
-
-	        } catch (IOException e) {
-
-	            System.out.println("파일이 없거나 읽는 중 오류 발생");
-	        }
+                System.out.print((char)data);
+            }
+            // 파일 닫기
+            fr.close();
+            System.out.println("\n파일 읽기 완료");
+        } catch (IOException e) {
+            System.out.println("파일이 없거나 읽는 중 오류 발생");
+        }finally {
+        		System.out.println("예외가 발생하든 안하든 필수적으로 실행해야될 코드 작성 공간");
+        }
 	}
 
 }
