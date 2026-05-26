7b3ce4fbfa22d8d610b7a1b40d3daceb6a5a1bea
kimdaechul <rlaeocjf@gmail.com>
2026-05-26 11:56:31 +0900

    commit

--- Java20260525_익명객체/src/ex06_3/Main.java
+++ Java20260525_익명객체/src/ex06_3/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		 * 람다식에서 return 이 있는 경우는 중괄호 생략불가
 		 * return이 포함된 중괄호에서 문장이 한 문장이면 return문 생략 및 중괄호도 생략 가능
 		 */
-		Calculable cal = (x, y) ->  x+y;
+		Calculable cal = (x, y) ->  x+y; 
 		
 		
 		int result = cal.calculate(5, 6);
